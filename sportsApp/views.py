b0ead1496c876651f4853e99837485bfbdb72e45
frankTheCodeBoy <olumfrank48@gmail.com>
2022-07-18 14:10:08 +0300

    revision on db

--- sportsApp/views.py
+++ sportsApp/views.py
@@ -13,7 +13,7 @@ from .forms import UserProfileForm, UserCommentForm
 class IndexView(View):
     def get(self, request):
         sports_list = Sport.objects.all()[:5]
-        other_sports = Sport.objects.all()[5:30]
+        other_sports = Sport.objects.all()[5:34]
         blog_list = SportBlog.objects.order_by('-date_published')[:2]
         second_blog = SportBlog.objects.order_by('-date_published')[2:4]
         third_blog = SportBlog.objects.order_by('-date_published')[4:6]
